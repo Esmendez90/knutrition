fa9d50f5e446ff922249975a7596d773c862cbb2
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2021-09-29 15:41:12 -0400

    added function to grab input values

--- js/index.js
+++ js/index.js
@@ -2,4 +2,21 @@ $(".navbar-collapse a").click(function () {
   $(".navbar-collapse").collapse("hide");
 });
 
+$("#searchBtn").click(function (event) {
+  event.preventDefault();
+  let inputCity = $("#inputCity").val().trim();
+  let chooseState = $("#inputState").val();
+  let zipcode = $("#inputZip").val();
+  let propType;
+  let propVal = document.getElementsByName("gridRadios");
 
+  for (let i = 0; i < propVal.length; i++) {
+    if (propVal[i].checked) propType = propVal[i].value;
+  }
+
+  apiCall(inputCity, chooseState, zipcode, propType);
+});
+
+function apiCall( inputCity, chooseState, zipcode, propType) {
+  console.log(inputCity, chooseState, zipcode, propType);
+}
