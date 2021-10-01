05160da34f9393353e3171f245b35b9e8573e26b
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2021-10-01 15:25:06 -0400

    added comments section

--- js/index.js
+++ js/index.js
@@ -1,22 +1,3 @@
 $(".navbar-collapse a").click(function () {
   $(".navbar-collapse").collapse("hide");
 });
-
-$("#searchBtn").click(function (event) {
-  event.preventDefault();
-  let inputCity = $("#inputCity").val().trim();
-  let chooseState = $("#inputState").val();
-  let zipcode = $("#inputZip").val();
-  let propType;
-  let propVal = document.getElementsByName("gridRadios");
-
-  for (let i = 0; i < propVal.length; i++) {
-    if (propVal[i].checked) propType = propVal[i].value;
-  }
-
-  apiCall(inputCity, chooseState, zipcode, propType);
-});
-
-function apiCall( inputCity, chooseState, zipcode, propType) {
-  console.log(inputCity, chooseState, zipcode, propType);
-}
