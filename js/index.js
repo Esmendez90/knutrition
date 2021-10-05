836fc8ead262ea341cd5d0047504bec7b1bbb7c5
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2021-10-05 14:35:08 -0400

    Added navbar pop up

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+let el = document.querySelector(".navbar-toggler");
+
 $(".navbar-collapse a").click(function () {
   $(".navbar-collapse").collapse("hide");
+});
+
+$("#navBarHeader").click(function () {
+  if (el.ariaExpanded === "true") {
+    $("main").css("display", "none");
+  } else {
+    $("main").css("display", "block");
+  }
 });
